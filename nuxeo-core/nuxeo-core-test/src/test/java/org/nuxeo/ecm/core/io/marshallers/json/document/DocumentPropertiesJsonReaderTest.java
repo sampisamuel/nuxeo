46b952a626b89f41a1dfd42c8b3bf68f3fca3b36
Kevin Leturc <kleturc@nuxeo.com>
2017-05-29 17:25:12 +0200

    NXP-22436: Retrieve correctly the schema definition while reading properties

--- nuxeo-core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/io/marshallers/json/document/DocumentPropertiesJsonReaderTest.java
+++ nuxeo-core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/io/marshallers/json/document/DocumentPropertiesJsonReaderTest.java
@@ -1,5 +1,6 @@
 /*
  * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+<<<<<<< HEAD
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -7,12 +8,25 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
+=======
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+>>>>>>> f565567f1ae... NXP-22436: Retrieve correctly the schema definition while reading properties
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+<<<<<<< HEAD
  *
+=======
+ *  
+>>>>>>> f565567f1ae... NXP-22436: Retrieve correctly the schema definition while reading properties
  * Contributors:
  *     Kevin Leturc <kleturc@nuxeo.com>
  */
@@ -46,7 +60,29 @@ public class DocumentPropertiesJsonReaderTest
     public DocumentPropertiesJsonReaderTest() {
         super(DocumentPropertyJsonWriter.class, List.class, TypeUtils.parameterize(List.class, Property.class));
     }
-    
+
+    /*
+     * NXP-22436
+     */
+    @Test
+    public void testReadSchemaWithoutPrefix() throws IOException {
+        String propertiesJson = "{ \"dc:title\": \"A note\",\n" + //
+                "  \"note:note\": \"note content\" }";
+
+        DocumentPropertiesJsonReader reader = registry.getInstance(CtxBuilder.get(), DocumentPropertiesJsonReader.class);
+        JsonParser jp = JsonFactoryProvider.get().createJsonParser(propertiesJson);
+        JsonNode jn = jp.readValueAsTree();
+        List<Property> properties = reader.read(jn);
+        assertNotNull(properties);
+        assertEquals(2, properties.size());
+        Schema titleSchema = properties.get(0).getSchema();
+        assertNotNull(titleSchema);
+        assertEquals("dublincore", titleSchema.getName());
+        Schema noteSchema = properties.get(1).getSchema();
+        assertNotNull(noteSchema);
+        assertEquals("note", noteSchema.getName());
+    }
+
     @Test
     public void testMultiValueDate() throws IOException {
         String propertiesJson = "{ \"dc:title\": \"A title\"," + //
@@ -65,4 +101,5 @@ public class DocumentPropertiesJsonReaderTest
         assertEquals(DateType.ID, listType.getName());
         assertEquals(DateType.ID, properties.get(2).getType().getName());
     }
+
 }
